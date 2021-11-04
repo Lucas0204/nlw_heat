1cd9e7ead9f6f90f2e6597461c3b75fc64e39ec9
Lucas Zordan <lucaszordan0204@gmail.com>
2021-11-03 21:33:11 -0300

    implementing web sockets to emit messages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import 'dotenv/config'
 import { app } from './app'
+import { Server } from 'socket.io'
 
 const port = process.env.APP_PORT
 
 const server = app.listen(port, () => console.log('server running...'))
+
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`User connected: ${socket.id}`)
+})
+
+export { server, io }
--- src/useCases/createMessage/createMessageService.ts
+++ src/useCases/createMessage/createMessageService.ts
@@ -1,10 +1,18 @@
 import { Message } from '../../model/Message'
+import { io } from '../../server'
 
 class CreateMessageService {
     async execute(message: string, user_id: string): Promise<Message> {
-        const newMessage = await Message.create({ message, user_id })
 
-        return newMessage
+        try {
+            const newMessage = await Message.create({ message, user_id })
+
+            io.emit('new_message', newMessage)
+
+            return newMessage
+        } catch(err) {
+            throw new Error(err.message)
+        }
     }
 }
 
